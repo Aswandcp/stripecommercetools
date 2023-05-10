acc956a1f7875df09eb7f034dfce162c7a7e1523
Aswandcp <aswand@sayonetech.com>
2023-05-10 16:07:56 +0530

    json errors solve

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,9 @@ export default function App() {
         headers: {
           "Content-Type": "application/json",
         },
+        body: JSON.stringify({
+          obj
+        }),
       })
         .then((res) => res.json())
         .then((data) => {
